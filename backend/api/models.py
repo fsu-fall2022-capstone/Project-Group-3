ad0d2be230140debffada77fa660516b61ea9e65
VFM20E <80614008+VFM20E@users.noreply.github.com>
2022-10-20 14:42:53 -0400

    Api upload along with a new name DataTweetApi
    API update with URLs, serialization, views, migration and models updated

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Users(models.Model):
+    Username = models.CharField(primary_key=True, max_length=32)
+    Password = models.CharField(max_length=32)
+    Email = models.CharField(max_length=50)
+    About = models.CharField(max_length=500)
+    RoleLvl = models.IntegerField()
+
+class Posts(models.Model):
+    PostID = models.IntegerField(primary_key=True)
+    PostFileName = models.CharField(max_length=64)
+    PostedBy = models.ForeignKey(Users, on_delete=models.CASCADE)
+    PostedWhen = models.DateTimeField()
+    Tags = models.CharField(max_length=500)
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,19 @@
+from django.template.defaulttags import url
 from django.urls import path
+import django.conf.urls
 from . import views
-
+from django.conf.urls.static import static
+from django.conf import settings
+#URLConf
 urlpatterns = [
-    path('/index', views.index, name='index')
-]
+
+    path('/index', views.index, name='index'),
+
+    url(r'^user$', views.usersApi),
+    url(r'^user/([0-9]+)$', views.usersApi),
+
+    url(r'^post$',views.postApi),
+    url(r'^post/([0-9]+)$',views.postApi),
+
+    url(r'^post/savefile',views.SaveFile)
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
