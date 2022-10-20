63cefa21fccadd1dcc6199564aedfec892b93d49
Tommy Chong <tommy242008@hotmail.com>
2022-10-20 00:19:31 -0400

    Re-commiting my changes as a pull request

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,7 +4,20 @@ import "../styles/home.css";
 function Home(){
     return(
         <div className="home">
-            Home
+            <div className = "card"> 
+                <div className = 'tweet'>
+                    <form>
+                    <textarea className='text-tweet' placeholder='Share something...'> 
+                    </textarea>
+                    <button type="submit">Post</button>
+                    </form>
+                </div>
+
+                <div className = 'posts'>
+                    
+                </div>
+
+            </div>
         </div>
     )
 }
