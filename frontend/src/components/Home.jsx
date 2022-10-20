7198993b16c622b685f46e36cd3d9e21d6937086
Tommy Chong <tommy242008@hotmail.com>
2022-10-20 00:17:03 -0400

    Deleting my files

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,20 +4,7 @@ import "../styles/home.css";
 function Home(){
     return(
         <div className="home">
-            <div className = "card"> 
-                <div className = 'tweet'>
-                    <form>
-                    <textarea className='text-tweet' placeholder='Share something...'> 
-                    </textarea>
-                    <button type="submit">Post</button>
-                    </form>
-                </div>
-
-                <div className = 'posts'>
-                    
-                </div>
-
-            </div>
+            Home
         </div>
     )
 }
