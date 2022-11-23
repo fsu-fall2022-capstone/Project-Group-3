74c051ebf9e532d7852c5823dc50ae2cf355c826
ocasodi <david.l.markel@gmail.com>
2022-11-23 17:51:18 -0500

    conditional log in/out

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/navbar.css";
-import LoginButton from './Login';
-import LogoutButton from './Logout';
+import LoginButton from "./Login";
+import LogoutButton from "./Logout";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export default function Navbar() {
+  const { isAuthenticated } = useAuth0();
   return (
     <nav className="nav">
       <Link to="/" className="site-name">
@@ -17,11 +19,11 @@ export default function Navbar() {
         <li>
           <Link to="/profile">Profile</Link>
         </li>
-        <li>
-          <LoginButton>Login</LoginButton>
-        </li>
-        <li>
-          <LogoutButton>Logout</LogoutButton>
+        <li className="log in/out">
+          {(() => {
+            if (isAuthenticated) return <LogoutButton>Logout</LogoutButton>;
+            else return <LoginButton>Login</LoginButton>;
+          })()}
         </li>
       </ul>
     </nav>
